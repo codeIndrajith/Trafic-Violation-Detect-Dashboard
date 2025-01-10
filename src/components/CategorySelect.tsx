6b374a946baaf97a0e89ebd1e341e6695e66fbf1
Indrajith <indrajith.bodinayaka352@gmail.com>
2025-01-10 11:42:16 +0530

    mobile screen responsive add

--- src/components/CategorySelect.tsx
+++ src/components/CategorySelect.tsx
@@ -4,19 +4,32 @@ import { MdDashboardCustomize } from "react-icons/md";
 import { SiPaypal } from "react-icons/si";
 import { IoCloseSharp } from "react-icons/io5";
 import Logo from "../images/Frame.png"
+import { useState } from "react";
+import { RiCloseLargeLine, RiMenu3Fill } from "react-icons/ri";
 
 const CategorySelect = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
+
+
+    const toggleSidebar = ():void => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
     return (
         <>
             {/* Left side section */}
-            <section className="absolute md:relative z-10 px-6 py-8 h-screen bg-red-300 w-full md:w-[35%] lg:w-1/5">
-                <div className="mb-24 flex items-center justify-between">
+            <section className="absolute md:relative z-10 px-6 py-8 h-screen w-full md:w-[35%] lg:w-1/5"> 
+                <div className="md:mb-24 flex items-center justify-between">
                     <img className="w-20 h-20" src={Logo} alt="logo" />
-                    <IoCloseSharp className="text-4xl md:hidden mr-4" />
+                    <RiMenu3Fill className="text-4xl md:hidden mr-4 cursor-pointer" onClick={toggleSidebar} />
+                
+                </div>
+                <div className="md:hidden mt-4">
+                    <h1 className="text-xl font-bold ml-4 pt-4">Dashboard</h1>
+                    <p className="text-[14px] text-[#131313] ml-4">Monday 03 March 2024</p>
                 </div>
 
 
-                <div className="">
+                <div className="hidden md:block">
                     <h1 className="text-md text-center md:text-start text-gray-400 mb-14 px-2">Category</h1>
                     <div className="flex flex-col gap-12 md:gap-8 px-2 w-full">
                         <div className="flex gap-8 items-center">
@@ -40,7 +53,38 @@ const CategorySelect = () => {
                         </div>
                     </div>
                 </div>
-            </section>
+            </section>       
+
+            {/* Mobile sidebar */}
+            {isSidebarOpen && (
+                <div className="md:hidden fixed px-6 py-8 inset-0 bg-gray-200 z-50 overflow-auto">
+                    <div className="mb-24 flex items-center justify-between">
+                        <img src={Logo} className="w-20 h-20" alt="logo image" />
+                        <RiCloseLargeLine className="text-4xl md:hidden mr-4 cursor-pointer" onClick={toggleSidebar} />
+                    </div>
+                    <div className="mt-24 ml-4 grid gap-8 items-center justify-center">
+                    <div className="flex gap-8 items-center">
+                            <MdDashboardCustomize className="text-lg" /> <span className="text-lg">Dashboard</span>
+                        </div>
+
+                        <div className="flex gap-8 items-center">
+                            <BsFillFileEarmarkRuledFill className="text-lg" /> <span className="text-lg">Rules</span>
+                        </div>
+
+                        <div className="flex gap-8 items-center">
+                            <BiSolidMobileVibration className="text-lg" /> <span className="text-lg">Violations</span>
+                        </div>
+
+                        <div className="flex gap-8 items-center">
+                            <BiSolidReport className="text-lg" /> <span className="text-lg">Reports</span>
+                        </div>
+
+                        <div className="flex gap-8 items-center">
+                            <SiPaypal className="text-lg" /> <span className="text-lg">Payment</span>
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,20 +7,23 @@ const Navbar = () => {
     return (
         <>
             {/* Right side section */}
-            <section className="absolute md:relative bg-blue-300 h-[120px] w-full md:w-4/5 flex flex-col md:flex-row items-center justify-between px-8">
-                <div>
-                    <h1 className="text-2xl font-bold">Dashboard</h1>
-                </div>
-
-                <div className="flex items-center gap-8">
-                    <div className="flex items-center gap-4">
-                        <AiOutlineMail className="text-lg" />
-                        <BsBell className="text-lg" />
-                        <p className="px-3 py-0.5 bg-gray-300 rounded-sm">I</p>
+            <section className="hidden md:block absolute md:relative bg-blue-300 h-[120px] w-full md:w-4/5">
+                <div className="flex flex-col md:flex-row items-center justify-between px-8 py-6">
+                    <div>
+                        <h1 className="text-2xl font-bold">Dashboard</h1>
+                        <p className="text-[12px] text-[#131313]">Monday 03 March 2024</p>
                     </div>
-                    <div className="flex items-center gap-1">
-                        <h1>Indrajith Bodhinayaka</h1>
-                        <MdKeyboardArrowDown className="text-xl" />
+
+                    <div className="flex items-center gap-8">
+                        <div className="flex items-center gap-4">
+                            <AiOutlineMail className="text-lg" />
+                            <BsBell className="text-lg" />
+                            <p className="px-3 py-0.5 bg-gray-300 rounded-sm">I</p>
+                        </div>
+                        <div className="flex items-center gap-1">
+                            <h1>Indrajith Bodhinayaka</h1>
+                            <MdKeyboardArrowDown className="text-xl" />
+                        </div>
                     </div>
                 </div>
             </section>
